38d9fd439f873c43978109144b57b33331b2a5b4
perl <perl@immunant.com>
2020-01-16 19:29:18 -0800

    Fix download condition

--- test_w3c.sh
+++ test_w3c.sh
@@ -3,8 +3,9 @@ set -e
 # download W3C test suite
 BUILD_DIR="$PWD"
 TEST_TMP=/tmp/libexpat
+TEST_SUITE=/tmp/libexpat/xml-test-suite
 ARCHIVE="$TEST_TMP/xmlts20020606.tar" 
-if [ ! -f "$ARCHIVE" ]; then
+if [ ! -d "$TEST_SUITE" ]; then
     mkdir -p "$TEST_TMP"
     curl http://www.w3.org/XML/Test/xmlts20020606.tar --silent --output "$ARCHIVE"
     tar -C "$TEST_TMP"  -xf "$ARCHIVE"
