e888aca997afae4d402f6a3c89b236086d1b5040
perl <perl@immunant.com>
2020-01-30 19:47:37 -0800

    Add a a few missing copyright notices

--- src/tests/chardata.rs
+++ src/tests/chardata.rs
@@ -23,6 +23,7 @@ use ::libc;
 
    Copyright (c) 1997-2000 Thai Open Source Software Center Ltd
    Copyright (c) 2000-2017 Expat development team
+   Portions copyright (c) 2020 Immunant, Inc.
    Licensed under the MIT license:
 
    Permission is  hereby granted,  free of charge,  to any  person obtaining
@@ -151,6 +152,7 @@ pub unsafe extern "C" fn CharData_AppendXMLChars(
 
    Copyright (c) 1997-2000 Thai Open Source Software Center Ltd
    Copyright (c) 2000-2017 Expat development team
+   Portions copyright (c) 2020 Immunant, Inc.
    Licensed under the MIT license:
 
    Permission is  hereby granted,  free of charge,  to any  person obtaining
