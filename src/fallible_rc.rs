19082e2de5dce243a177c1a3674c67ec16784c46
Andrei Homescu <ah@immunant.com>
2020-03-25 20:25:44 -0700

    Simplify the transmute code in Rc::try_new

--- src/fallible_rc.rs
+++ src/fallible_rc.rs
@@ -1,6 +1,5 @@
 use std::cell::Cell;
 use std::collections::TryReserveError;
-use std::ptr::NonNull;
 use std::rc::Rc;
 
 use fallible_collections::FallibleBox;
@@ -23,13 +22,9 @@ impl<T> FallibleRc<T> for Rc<T> {
             value,
         };
         let b = Box::try_new(inner)?;
-        match NonNull::new(Box::into_raw(b)) {
-            Some(inner) => unsafe {
-                // WARNING: really unsafe!!!
-                Ok(std::mem::transmute(inner))
-            }
-            // FIXME: error should be different
-            None => Err(TryReserveError::CapacityOverflow)
+        unsafe {
+            // WARNING: really unsafe!!!
+            Ok(std::mem::transmute(b))
         }
     }
 }
